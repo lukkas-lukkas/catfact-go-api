5ec57889e1816fad533990364ff81bee9a54b7d1
Lucas Lima <lukkasoliveiralima@gmail.com>
2024-04-25 22:54:52 -0300

    change the output

--- main.go
+++ main.go
@@ -10,10 +10,10 @@ func main () {
 	catService := NewGetFactService("https://catfact.ninja/fact")
 	logger := NewLoggingService(catService)
 
-	fact, err := logger.GetCatFact(context.TODO())
+	_, err := logger.GetCatFact(context.TODO())
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("%+v\n", fact)
+	fmt.Println("Finished")
 }
